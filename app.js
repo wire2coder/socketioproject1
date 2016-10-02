255e07cf9c4b6dc71f0270ce97d7b1fdb7874d3e
Terry <opsvm3user@opsvm3.turner.com>
2016-10-02 13:59:49 -0400

    Confused with socket.on and client.on >> app.js

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 // make an Express web server
 
+var mongo = require('mongodb').MongoClient;
+
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 
+// creating a socket instance
+var socket = require('socket.io')(server);
 
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-
-
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
 
+mongo.connect('mongodb://localhost/mongochat', function (err, db) {
+    if(err) {
+        throw err;
+    }
 
+    // Add a connect listener
+    socket.on('connection', function(socket) {
 
+        // Success! Now listen to messages to be received
+
+        var chat = db.collection('chats');
+        chat.find().limit(100).sort({_id: 1}).toArray(function (err, res) {
+            if (err) {
+                throw err;
+            }
+        });
+    }); // socket.on
+});
+
+/*
 app.get('/', function(req, res) {
     res.sendFile('index.html');
 });
-
-io.on('connection', function (socket) {
-    //console.log('a user is connected');
-
-    /*
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-    */
-
-    // from the client, index.html 'chat message'
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
-    });
-
-});
-
-
-
-
+*/
 
 server.listen(3000);
 console.log('Server started on port 3000');
