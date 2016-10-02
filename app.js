c28fd04a81526a0061aecef28a51aeac96f84f57
Terry <opsvm3user@opsvm3.turner.com>
2016-10-02 12:52:51 -0400

    Socket IO Skeleton code with socket.on, io.emit

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
+
 
 
 app.get('/', function(req, res) {
@@ -27,7 +31,7 @@ io.on('connection', function (socket) {
 
     // from the client, index.html 'chat message'
     socket.on('chat message', function (msg) {
-        console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
 
 });
