c082400f3bb9aa17cb8a9450760da820e7f108f6
Terry <opsvm3user@opsvm3.turner.com>
2016-10-03 13:56:39 -0400

    Send Mongo response to the client, look for HERE

--- app.js
+++ app.js
@@ -14,20 +14,30 @@ var methodOverride = require('method-override');
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
 
-socket.on('connection', function (client) {
-    console.log('Server: client is connected');
+mongo.connect('mongodb://localhost/mongochat', function (err, db) {
+    if (err) { throw err; }
+    console.log('Connected the Mongo');
 
-    // Listening for event on 'message'
-    client.on('message', function (data) {
-        console.log('Received message from client!', data.m1);
+    var chat = db.collection('chats');
+    chat.find().limit(100).toArray(function (err, response) {
+        //console.log(response);
 
+        // send data to the client on the channel 'output'
+        console.log('Server: sending response to the client');
 
+        HERE
     });
 
-    client.emit('message', 'jimjim');
+    socket.on('connection', function (client) {
+        console.log('Server: client is connected');
 
+        // Listening for event on 'message'
+        client.on('message', function (data) {
+            console.log('Received message from client!', data);
+        });
+        //client.emit('message', 'jimjim');
+    });
 });
-
 
 server.listen(3000);
 console.log('Server running on port 3000');
