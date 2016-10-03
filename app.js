5f899d2e4ef9e21f956e530944fccff72896d577
Terry <opsvm3user@opsvm3.turner.com>
2016-10-03 15:22:07 -0400

    commit macro

--- app.js
+++ app.js
@@ -18,26 +18,41 @@ mongo.connect('mongodb://localhost/mongochat', function (err, db) {
     if (err) { throw err; }
     console.log('Connected the Mongo');
 
-    var chat = db.collection('chats');
-    chat.find().limit(100).toArray(function (err, response) {
-        //console.log(response);
-
-        // send data to the client on the channel 'output'
-        console.log('Server: sending response to the client');
-
-        HERE
-    });
-
     socket.on('connection', function (client) {
         console.log('Server: client is connected');
 
+        var chat = db.collection('chats');
+        chat.find().limit(100).toArray(function (err, response) {
+            console.log(response);
+
+            // send data to the client on the channel 'output'
+            console.log('Server: sending response to the client');
+            client.emit('output', response);
+        });
+
+        client.on('input', function (data) {
+            var name = data.name;
+            var message = data.message;
+
+            if(name === '' || message =='') {
+                // make user insert name and messages
+                console.log('Please fill in name and messages');
+            } else {
+                // insert name and messages into MongoDB
+                chat.insert({}, function () {
+                    HERE, WORK ON INDEX.HTML
+                });
+            }
+
+        });
+
+
         // Listening for event on 'message'
         client.on('message', function (data) {
-            console.log('Received message from client!', data);
+            console.log('Server: Received message from client!', data);
         });
-        //client.emit('message', 'jimjim');
-    });
-});
+    }); // socket.on('connection'
+}); // mongo.connect
 
 server.listen(3000);
 console.log('Server running on port 3000');
