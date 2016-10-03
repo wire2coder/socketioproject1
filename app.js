eeb746c0037e5871b6dd8dc419abc4073c459958
Terry <opsvm3user@opsvm3.turner.com>
2016-10-03 11:06:08 -0400

    a little more clear on client.on and client.emit

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // make an Express web server
-
-var mongo = require('mongodb').MongoClient;
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+
+var mongo = require('mongodb').MongoClient;
 
 // creating a socket instance
 var socket = require('socket.io')(server);
@@ -15,32 +14,20 @@ var methodOverride = require('method-override');
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
 
-mongo.connect('mongodb://localhost/mongochat', function (err, db) {
-    if(err) {
-        throw err;
-    }
+socket.on('connection', function (client) {
+    console.log('Server: client is connected');
 
-    // Add a connect listener
-    socket.on('connection', function(socket) {
+    // Listening for event on 'message'
+    client.on('message', function (data) {
+        console.log('Received message from client!', data.m1);
 
-        // Success! Now listen to messages to be received
 
-        var chat = db.collection('chats');
-        chat.find().limit(100).sort({_id: 1}).toArray(function (err, res) {
-            if (err) {
-                throw err;
-            }
-        });
-    }); // socket.on
+    });
+
+    client.emit('message', 'jimjim');
+
 });
 
-/*
-app.get('/', function(req, res) {
-    res.sendFile('index.html');
-});
-*/
 
 server.listen(3000);
-console.log('Server started on port 3000');
-
-
+console.log('Server running on port 3000');
