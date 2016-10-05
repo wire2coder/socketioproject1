dd01cf93ae07ddd96cd0f0ba2057fb4e9c7f52d0
Terry <opsvm3user@opsvm3.turner.com>
2016-10-05 18:40:56 -0400

    Everything works, use this as a template for socketio

--- app.js
+++ app.js
@@ -19,43 +19,60 @@ mongo.connect('mongodb://localhost/mongochat', function (err, db) {
     console.log('Connected the Mongo');
 
     socket.on('connection', function (client) {
-        console.log('Server: client is connected');
+        console.log('a client is connected');
 
         var chat = db.collection('chats');
-        chat.find().limit(100).toArray(function (err, response) {
-            console.log(response);
 
-            // send data to the client on the channel 'output'
-            console.log('Server: sending response to the client');
-            client.emit('output', response);
-        });
-
-        client.on('input', function (data) {
-            var name = data.name;
-            var message = data.message;
-
-            if(name === '' || message =='') {
-                // make user insert name and messages
-                client.emit('status', 'Please enter a name and a message');
-
-            } else {
-                // insert name and messages into MongoDB
-                //chat.insert({name: name, message: message}, function () {
-                //    console.log('Server: stuff inserted');
-                //});
-
-                client.emit('output', [data])
+        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+            if(err){
+                throw err
             }
 
+            res.forEach(function (asdf) {
+                // console.log(asdf.name + ': ' + asdf.message);
+                // var x = asdf.name + ': ' + asdf.message;
+                client.emit('chat', {name: asdf.name, message: asdf.message});
+            });
+
         });
 
 
-        // Listening for event on 'message'
-        client.on('message', function (data) {
-            console.log('Server: Received message from client!', data);
+        client.on('disconnect', function () {
+            console.log(' a client is DISCONNECTED');
         });
-    }); // socket.on('connection'
+
+
+        client.on('chat message', function (data) {
+
+            chat.insert({name: data.name, message: data.message}, function () {
+                console.log('Server: stuff inserted');
+
+                client.broadcast.emit('chat', data);
+                client.emit('chat', data);
+            });
+
+        });
+
+        client.on('clear', function (data) {
+            console.log('Server: clear');
+
+            chat.remove({}, function (err) {
+                if (err) {
+                    throw err;
+                }
+
+                console.log('Server: I cleared chat collections');
+            });
+        });
+
+    });
+
 }); // mongo.connect
+
+
+
+
+
 
 server.listen(3000);
 console.log('Server running on port 3000');
